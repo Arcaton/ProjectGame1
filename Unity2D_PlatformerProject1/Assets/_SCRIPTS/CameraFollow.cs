9a05fd5076f2bb5d11bff7cf12709d92987bb782
Arcaton <162802@chrome.escg.ac.uk>
2019-11-04 15:24:22 +0000

    trail and player jumping fix. Camera moves

---
+++ Unity2D_PlatformerProject1/Assets/_SCRIPTS/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+
+    public float smoothTimeY;
+    public float smoothTimeX;
+
+    public GameObject player;
+
+    void Start ()
+    {
+
+        player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + 10, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y + 2, ref velocity.y, smoothTimeY);
+
+        transform.position = new Vector3(posX, posY , transform.position.z);
+
+    }
+}
