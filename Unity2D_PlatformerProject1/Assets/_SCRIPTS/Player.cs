2b0710ca111220a88e1405e67ec4cd58772a33a5
Arcaton <162802@chrome.escg.ac.uk>
2019-11-11 14:33:05 +0000

    player movement major issues fixed

--- Unity2D_PlatformerProject1/Assets/_SCRIPTS/Player.cs
+++ Unity2D_PlatformerProject1/Assets/_SCRIPTS/Player.cs
@@ -5,18 +5,26 @@ public class Player : MonoBehaviour
 {
 
     public float maxSpeed = 3;
-    public float speed = 50f;
-    public float jumpPower = 400f;
+    public float speed = 300f;
+    public float jumpPower = 800f;
 
-    public bool grounded;
+     bool grounded = true;
 
     private Rigidbody2D rb2d;
 
     void Start()
     {
         rb2d = gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log("jsfhjksdhgf");
     }
 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "ground")
+        {
+            grounded = true;
+        }
+    }
 
     void Update()
     {
@@ -31,13 +39,16 @@ public class Player : MonoBehaviour
         //Moving the player
         rb2d.AddForce((Vector2.right * speed) * h);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && grounded == true)
         {
             rb2d.AddForce(Vector2.up * jumpPower);
+            grounded = false;
         }
 
-            //Limiting the speed of the player
-            if (rb2d.velocity.x > maxSpeed)
+
+
+        //Limiting the speed of the player
+        if (rb2d.velocity.x > maxSpeed)
         {
             rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
         }
