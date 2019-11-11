a35a6cfc527298af2014fa29fa12891eb95dbfd9
Arcaton <162802@chrome.escg.ac.uk>
2019-11-11 16:27:06 +0000

    Added Pause menu

---
+++ Unity2D_PlatformerProject1/Assets/_SCRIPTS/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseUI;
+
+    //creating a private bool so nothing but this script can access the bool.
+    private bool paused = false;
+
+    void Start()
+    {
+        //setting it so that when we start the game the pause menu is disabled.
+        PauseUI.SetActive(false);
+
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            //this is togglight the bool "paused". Press one time = enabled, press again = disabled.
+            paused = !paused;
+        }
+
+        //if the game is paused
+        if (paused)
+        {
+            //enble the pause UI
+            PauseUI.SetActive(true);
+            //this is setting the time to 0 so when the game is paused time is not progressing; Nothing is happening.
+            Time.timeScale = 0;
+        }
+        //if the game is not paused
+        if (!paused)
+        {
+            //hide (disable) the PauseUI
+            PauseUI.SetActive(false);
+            //this is setting the time to 1. 1 because 1 is default time progression aka normal
+            Time.timeScale = 1;
+        }
+    }
+}
